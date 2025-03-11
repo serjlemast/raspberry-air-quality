94ca9e3936c79da9020bfed1207dbcf7745a914b
malexj <alexmaximovj@gmail.com>
2025-03-11 22:34:29 +0200

    test

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 * Java 23
 * Springboot 3.4.3
-* Gradle 8.12
+* Gradle 8.13
 
 #### Project configuration
 
--- src/main/java/com/serjlemast/scheduler/SchedulerProcessor.java
+++ src/main/java/com/serjlemast/scheduler/SchedulerProcessor.java
@@ -1,5 +1,8 @@
 package com.serjlemast.scheduler;
 
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.platform.Platforms;
 import com.serjlemast.model.SensorDataEvent;
 import com.serjlemast.publisher.RabbitMqPublisher;
 import com.serjlemast.service.SensorService;
@@ -18,8 +21,17 @@ public class SchedulerProcessor {
   private final RabbitMqPublisher publisher;
   private final List<SensorService> sensorServices;
 
+  private final Context pi4j = Pi4J.newAutoContext();
+
   @Scheduled(cron = "${scheduled.cron}")
   public void process() {
+
+    Platforms platforms = pi4j.platforms();
+
+    int PIN_4 = 4;
+
+    var out = pi4j.digitalOutput().create(PIN_4);
+
     var timestamp = LocalDateTime.now();
     var threadName = Thread.currentThread().getName();
     log.info("Starting sensor data processing at {} on thread: {}", timestamp, threadName);
