d60f397e3dfca2357dca8e46d52246c70496bdfc
malexj <alexmaximovj@gmail.com>
2025-03-12 14:05:25 +0200

    test5

--- src/main/java/com/serjlemast/scheduler/SchedulerProcessor.java
+++ src/main/java/com/serjlemast/scheduler/SchedulerProcessor.java
@@ -12,9 +12,11 @@ import com.serjlemast.service.SensorService;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -26,14 +28,19 @@ public class SchedulerProcessor {
 
   private static final int GPIO_PIN_4 = 4;
 
+  private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
   //
   //  private Context pi4j = Pi4J.newAutoContext();
 
-  public SchedulerProcessor(RabbitMqPublisher publisher, List<SensorService> sensorServices) {
+  public SchedulerProcessor(RabbitMqPublisher publisher, List<SensorService> sensorServices,
+                            ThreadPoolTaskExecutor threadPoolTaskExecutor) {
     this.publisher = publisher;
     this.sensorServices = sensorServices;
+    this.threadPoolTaskExecutor = threadPoolTaskExecutor;
   }
 
+  @SneakyThrows
   public void test() {
     Context pi4j = Pi4J.newAutoContext();
     log.info(" >>> 1 Starting SchedulerProcessor");
@@ -70,7 +77,7 @@ public class SchedulerProcessor {
                 .id("DHT11_INPUT")
                 .name("DHT11 Input")
                 .address(GPIO_PIN_4) // Same GPIO pin
-                .pull(PullResistance.OFF) // No pull-up or pull-down resistor
+//                .pull(PullResistance.OFF) // No pull-up or pull-down resistor
                 .build());
 
     // Wait for response from DHT11 sensor
@@ -79,19 +86,23 @@ public class SchedulerProcessor {
 //    while (input.state() == com.pi4j.io.gpio.digital.DigitalState.HIGH) {}
 
 
-    log.info(" >>> 3 Starting SchedulerProcessor");
+
+
+    log.info(" >>> 3 Starting SchedulerProcessor, input.state() - {}", input.state());
 
     // Read 40 bits of data (Temperature and Humidity)
     int[] data = new int[40];
     for (int i = 0; i < 40; i++) {
 //      while (input.state() == com.pi4j.io.gpio.digital.DigitalState.LOW) {}
+      TimeUnit.MICROSECONDS.sleep(100);
       long startTime = System.nanoTime();
+      TimeUnit.MICROSECONDS.sleep(100);
 //      while (input.state() == com.pi4j.io.gpio.digital.DigitalState.HIGH) {}
       long pulseTime = System.nanoTime() - startTime;
       data[i] = (pulseTime > 50000) ? 1 : 0; // 1 if pulse > 50ms, else 0
     }
 
-    log.info(" >>> 3.1 Starting SchedulerProcessor - {}", Arrays.toString(data));
+    log.info(" >>> 3.1 Starting SchedulerProcessor, input.state()- {}, data - {}",input.state(), Arrays.toString(data));
 
     // Decode the data
     int humidityInt = bitsToByte(data, 0);
@@ -104,11 +115,11 @@ public class SchedulerProcessor {
 
     int calculatedChecksum = humidityInt + humidityDec + temperatureInt + temperatureDec;
     if ((calculatedChecksum & 0xFF) == checksum) {
-      System.out.println("✅ Data received successfully:");
-      System.out.println("🌡 Temperature: " + temperatureInt + "." + temperatureDec + "°C");
-      System.out.println("💧 Humidity: " + humidityInt + "." + humidityDec + "%");
+      System.out.println("Data received successfully:");
+      System.out.println("Temperature: " + temperatureInt + "." + temperatureDec + "°C");
+      System.out.println("Humidity: " + humidityInt + "." + humidityDec + "%");
     } else {
-      System.err.println("❌ Checksum error!");
+      System.err.println("Checksum error!");
     }
 
     // Shutdown Pi4J context
@@ -126,7 +137,7 @@ public class SchedulerProcessor {
 
   @SneakyThrows
 //    @Scheduled(cron = "${scheduled.cron}")
-  @Scheduled(cron = "0 */1  * * * *")
+  @Scheduled(cron = "*/10 *  * * * *")
   public void process() {
 
     test();
