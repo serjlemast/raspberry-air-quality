32519ced542036c773f893d00e8e37f5814f0538
malexj <alexmaximovj@gmail.com>
2025-03-12 20:44:21 +0200

    tag.0.0.1-release

--- src/main/java/com/serjlemast/gpio/Gpio4Reader.java
+++ src/main/java/com/serjlemast/gpio/Gpio4Reader.java
@@ -53,8 +53,8 @@ public class Gpio4Reader {
       }
 
       String jsonResponse = output.toString().trim();
-      if (jsonResponse.isEmpty()) {
-        log.warn("Empty response from Python script");
+      if (jsonResponse.isEmpty() || jsonResponse.contains("A full buffer was not returned.")) {
+        log.warn("Empty response from Python script or full buffer was not returned");
         return Optional.empty();
       }
 
