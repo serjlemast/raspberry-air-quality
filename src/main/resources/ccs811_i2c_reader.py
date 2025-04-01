cdfe3f29be18b4f63f4b3704c04ab6a3bcd1c2c7
malexj <alexmaximovj@gmail.com>
2025-04-01 23:16:10 +0300

    Update py script

--- src/main/resources/ccs811_i2c_reader.py
+++ src/main/resources/ccs811_i2c_reader.py
@@ -1,20 +1,10 @@
-import time
-import board
-import adafruit_ccs811
 import json
+import random
 
-# Initialize CCS811 sensor
-i2c = board.I2C()
-ccs811 = adafruit_ccs811.CCS811(i2c)
-
-# Wait for the sensor to be ready
-while not ccs811.data_ready:
-    pass
-
-def read_ccs811_data():
+def generate_mock_ccs811_data():
     try:
-        eco2 = ccs811.eco2
-        tvoc = ccs811.tvoc
+        tvoc = random.randint(0, 600)
+        eco2 = random.randint(400, 2000)
 
         data = {
             "tvoc": tvoc,
@@ -22,12 +12,9 @@ def read_ccs811_data():
         }
 
         return json.dumps(data)
-
-    except RuntimeError as error:
-        return json.dumps({"error": str(error)})
     except Exception as error:
         return json.dumps({"error": "Unexpected error: " + str(error)})
 
-if __name__ == "__main__":
-    result = read_ccs811_data()
+if name == "main":
+    result = generate_mock_ccs811_data()
     print(result)
